af2fc5dc1b033a4dbc7cc266acdb65354a4eabcc
akash1kumar20 <127503323+akash1kumar20@users.noreply.github.com>
2024-05-28 16:58:06 +0530

    Tailwindcss_based_project
    Amazon clone with the help of Tailwindcss.

---
+++ amazon_clone_tailwind/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./distr/*.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "hero-Image": "url('/../distr/Image/61zAjw4bqPL._SX3000_.jpg')",
+      },
+    },
+  },
+  plugins: [],
+};
